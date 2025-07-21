614a0e03fd927665b04b625ae0b837040edb934f
djjay7 <vaxinefree@gmail.com>
2025-07-21 09:37:40 +0800

    Add files via upload

---
+++ deepseek_javascript_20250721_da774c.js
@@ -0,0 +1,9 @@
+// CTA Button Animation
+document.getElementById("cta-button").addEventListener("click", function() {
+    alert("You summoned Satoshi’s Ghost. $GHOST is watching...");
+});
+
+// Fake "Satoshi is typing..." Easter Egg
+setTimeout(() => {
+    console.log("Satoshi is typing...");
+}, 5000);
